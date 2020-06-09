365450689e15057d78667c0263ca6b5ec4ad43e1
marimonon <mappi-@hotmail.co.jp>
2020-06-09 07:03:06 +0000

    facebook qiita compleate

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
   # deviseの後ろに書いてあるのはモジュール
+  
+  # facebook実装用
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        email:    auth.info.email,
+        name:  auth.info.name,
+        password: Devise.friendly_token[0, 20],
+        image:  auth.info.image
+      )
+    end
+
+    user
+  end
 end
