66fcf5bddc6d28283a6206b5b8e85e100a977212
marimonon <mappi-@hotmail.co.jp>
2020-06-13 15:02:20 +0000

    profile_page modify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
-  #　ストロングパラメーター対策（:nameの追加）
-  # protect_from_forgery with: :exception
+  # 　ストロングパラメーター対策（:nameの追加）
+  protect_from_forgery with: :exception
 
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # protected
+  protected
 
-  #   def configure_permitted_parameters
-  #     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  #     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  #   end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
 end
